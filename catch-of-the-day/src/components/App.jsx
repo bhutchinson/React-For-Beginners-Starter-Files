ca5b6c5fdb0abb3b390adc47d91665e24c5234b0
Bryan Hutchinson <bryan@kuali.co>
2018-03-28 09:21:05 -0400

    lesson 8

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -9,7 +9,6 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
-          <Header tagline="Bryan is Cool!" />
         </div>
         <Order />
         <Inventory />
